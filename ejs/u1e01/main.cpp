6519b0ee55146601575a79f6a2f99f21010bf23f
Matias Volpe <kratico@users.noreply.github.com>
2024-03-11 11:17:15 -0300

    agrega u1e01

--- ejs/u1e01/main.cpp
+++ ejs/u1e01/main.cpp
@@ -1,6 +1,33 @@
+#include <cassert>
 #include <iostream>
+
+int calcularSaltos(int x, int y, int d);
 
 int main()
 {
-  std::cout << "u1e01" << std::endl;
+  // casos de prueba
+  assert(2 == calcularSaltos(1, 6, 3));
+  assert(0 == calcularSaltos(1, 1, 3));
+  assert(3 == calcularSaltos(0, 9, 3));
+
+  int x;
+  int y;
+  int d;
+  std::cout << "ingrese x: ";
+  std::cin >> x;
+  std::cout << "ingrese y: ";
+  std::cin >> y;
+  std::cout << "ingrese d: ";
+  std::cin >> d;
+
+  std::cout << "tiene que saltar " << calcularSaltos(x, y, d) << "\n";
+}
+
+int calcularSaltos(int x, int y, int d)
+{
+  assert(x <= y);
+  assert(d > 0);
+
+  int saltoExtra = ((y - x) % d) > 0 ? 1 : 0;
+  return ((y - x) / d) + saltoExtra;
 }
