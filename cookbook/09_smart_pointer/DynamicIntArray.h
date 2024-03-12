f11150b98b075d0f522bb7eb13c887e02b6d7628
Matias Volpe <kratico@users.noreply.github.com>
2024-03-12 09:13:34 -0300

    feat: add DynamicIntArray with smart pointers

---
+++ cookbook/09_smart_pointer/DynamicIntArray.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstddef>
+#include <iostream>
+#include <memory>
+
+namespace cookbook {
+
+class DynamicIntArray {
+private:
+  std::unique_ptr<int[]> m_data;
+  std::size_t m_size;
+
+public:
+  DynamicIntArray(std::size_t);
+
+  // See https://en.wikipedia.org/wiki/Rule_of_three_(C%2B%2B_programming)
+  //  ~DynamicIntArray();
+  DynamicIntArray(const DynamicIntArray&);
+  DynamicIntArray& operator=(DynamicIntArray); // note: argument passed by value
+
+  //  DynamicIntList& operator=(const DynamicIntList&); // note: argument
+  //  passed by reference
+
+  void swap(DynamicIntArray&);
+
+  bool operator==(const DynamicIntArray& other) const;
+  int& operator[](int index);
+  int operator[](int index) const;
+
+  std::size_t get_size();
+
+  friend std::ostream& operator<<(std::ostream& os, const DynamicIntArray&);
+};
+
+} // namespace cookbook
