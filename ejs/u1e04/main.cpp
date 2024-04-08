249558c3a91e90c5c67db233d455dcbb796efa45
Matias Volpe <kratico@users.noreply.github.com>
2024-04-08 09:34:02 -0300

    feat: agrega u1e04

--- ejs/u1e04/main.cpp
+++ ejs/u1e04/main.cpp
@@ -1,6 +1,23 @@
+#include <cassert>
 #include <iostream>
+
+// 5! = 5 * 4 * 3 * 2 * 1
+// 4! = 4 * 3 * 2 * 1
+
+// 5! = 5 * 4!
+// n! = n * (n-1)!
+
+int factorial(int n)
+{
+  assert(n >= 0);
+  if (n == 1 || n == 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
 
 int main()
 {
-  std::cout << "u1e04" << std::endl;
+  std::cout << "factorial(3) = " << factorial(3) << std::endl;
+  std::cout << "factorial(-1) = " << factorial(-1) << std::endl;
 }
