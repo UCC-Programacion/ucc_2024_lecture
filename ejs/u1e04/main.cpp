403f634a67861d91409b1842c77bd4983e21cca9
Matias Volpe <kratico@users.noreply.github.com>
2024-04-08 09:41:36 -0300

    feat: agrega u1e04 con for

--- ejs/u1e04/main.cpp
+++ ejs/u1e04/main.cpp
@@ -16,8 +16,18 @@ int factorial(int n)
   return n * factorial(n - 1);
 }
 
+int factorial_for(int n)
+{
+  assert(n >= 0);
+  int out = 1;
+  for (int i = 1; i <= n; ++i) {
+    out = out * i;
+  }
+  return out;
+}
+
 int main()
 {
   std::cout << "factorial(3) = " << factorial(3) << std::endl;
-  std::cout << "factorial(-1) = " << factorial(-1) << std::endl;
+  std::cout << "factorial(3) = " << factorial_for(3) << std::endl;
 }
