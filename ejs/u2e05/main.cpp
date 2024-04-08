c5adbb80ceb106bc84c50edff54d72a5322a250e
Matias Volpe <kratico@users.noreply.github.com>
2024-04-08 11:26:44 -0300

    feat: agrega u2e05

--- ejs/u2e05/main.cpp
+++ ejs/u2e05/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+char* transformar_a_mayusculas(char* chars)
+{
+  char* c = chars;
+  while (*c != '\0') {
+    // convertir a mayusculas
+    if (*c >= 97 && *c <= 122)
+      *c = *c - 32;
+    //    int char_en_decimal = *c;
+    //    std::cout << *c << " = " << char_en_decimal << "\n";
+    c++;
+  }
+  return chars;
+}
+
 int main()
 {
   std::cout << "u2e05" << std::endl;
+
+  char c[] = "abc1deGhI";
+
+  std::cout << c << "\n";
+  std::cout << transformar_a_mayusculas(c) << "\n";
 }
