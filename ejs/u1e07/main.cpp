0c203f8b842e517072203af8c32bd83627d3d370
Matias Volpe <kratico@users.noreply.github.com>
2024-04-08 09:58:44 -0300

    feat: agrega u1e07

--- ejs/u1e07/main.cpp
+++ ejs/u1e07/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+void intercambiar(int& a, int& b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+void direccion_por_copia(int a)
+{
+  std::cout << "copia &a = " << &a << "\n";
+}
+
+void direccion_por_referencia(int& a)
+{
+  std::cout << "referencia &a = " << &a << "\n";
+}
+
 int main()
 {
   std::cout << "u1e07" << std::endl;
+
+  int x = 5;
+  int y = 7;
+
+  std::cout << "antes\n";
+  std::cout << "x = " << x << "\n";
+  std::cout << "y = " << y << "\n";
+
+  intercambiar(x, y);
+
+  std::cout << "despues\n";
+  std::cout << "x = " << x << "\n";
+  std::cout << "y = " << y << "\n";
+
+  std::cout << "en main &x = " << &x << "\n";
+  direccion_por_copia(x);
+  direccion_por_referencia(x);
 }
