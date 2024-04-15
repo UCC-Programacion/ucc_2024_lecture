336c7038fe5785c730d49c3daceb6f6bd0e4a232
Matias Volpe <kratico@users.noreply.github.com>
2024-04-15 09:58:11 -0300

    feat: agrega u2e12

--- ejs/u2e12/main.cpp
+++ ejs/u2e12/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+float** crear_matriz(int filas, int columnas)
+{
+  float** matriz = new float*[filas];
+  for (int i = 0; i < filas; ++i) {
+    matriz[i] = new float[columnas] {};
+  }
+  return matriz;
+}
+
 int main()
 {
   std::cout << "u2e12" << std::endl;
+
+  const int columnas = 3;
+  const int filas = 2;
+  float** matriz = crear_matriz(filas, columnas);
+
+  int n = 0;
+  for (int i = 0; i < filas; ++i) {
+    for (int j = 0; j < columnas; ++j) {
+      matriz[i][j] = n++;
+      // std::cin >> matriz[i][j];
+    }
+  }
+
+  for (int i = 0; i < filas; ++i) {
+    for (int j = 0; j < columnas; ++j) {
+      std::cout << matriz[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+
+  for (int i = 0; i < filas; ++i) {
+    delete[] matriz[i];
+  }
+
+  delete[] matriz;
 }
