aabc13e69fcd3bef0c8acbd035f0151f6c0eb42e
Matias Volpe <kratico@users.noreply.github.com>
2024-04-15 09:31:23 -0300

    feat: agrega u2e09

--- ejs/u2e09/main.cpp
+++ ejs/u2e09/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+int* crear_array(int size)
+{
+  int* out = new int[size] {};
+  return out;
+}
+
 int main()
 {
   std::cout << "u2e09" << std::endl;
+
+  const int size = 5;
+  int* a = crear_array(size);
+
+  for (int i = 0; i < size; ++i) {
+    a[i] = i * 2;
+  }
+
+  for (int i = 0; i < size; ++i) {
+    std::cout << "a[i] = " << a[i] << " *(a + i) = " << *(a + i) << "\n";
+  }
+  std::cout << "\n";
+
+  delete[] a;
 }
