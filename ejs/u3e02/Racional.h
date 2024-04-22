14319ac4bab5d7a218068261ffe6d76d36b98f3d
Matias Volpe <kratico@users.noreply.github.com>
2024-04-22 20:50:35 -0300

    feat: completa u3e02

--- ejs/u3e02/Racional.h
+++ ejs/u3e02/Racional.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cassert>
+#include <iostream>
+#include <numeric>
 
 class Racional {
 private:
@@ -32,13 +34,73 @@ public:
   //    assert(denominador != 0);
   //  }
 
-  int get_numerador()
+  int get_numerador() const
   {
     return m_numerador;
   }
 
-  int get_denominador()
+  int get_denominador() const
   {
     return m_denominador;
   }
+
+  Racional& operator++()
+  {
+    m_numerador += m_denominador;
+    return *this;
+  }
+
+  Racional operator++(int)
+  {
+    Racional temp = *this;
+    m_numerador += m_denominador;
+    return temp;
+  }
+
+  // TODO: implementar pre/post decrementar
+
+  Racional simplificar() const
+  {
+    int gcd = std::gcd(m_numerador, m_denominador);
+    return Racional { m_numerador / gcd, m_denominador / gcd };
+  };
+
+  //  void simplificar()
+  //  {
+  //    int gcd = std::gcd(m_numerador, m_denominador);
+  //    m_numerador = m_numerador / gcd;
+  //    m_denominador = m_denominador / gcd;
+  //  };
 };
+
+//  1/2 + 3/4
+// = (1*4 + 2*3) / 2*4
+Racional operator+(const Racional& r1, const Racional& r2)
+{
+  int numerador = r1.get_numerador() * r2.get_denominador() + r1.get_denominador() * r2.get_numerador();
+  int denominador = r1.get_denominador() * r2.get_denominador();
+  Racional out { numerador, denominador };
+  return out;
+}
+
+Racional operator+(const Racional& r1, int n)
+{
+  int numerador = r1.get_numerador() * 1 + r1.get_denominador() * n;
+  int denominador = r1.get_denominador() * 1;
+  Racional out { numerador, denominador };
+  return out;
+}
+
+Racional operator-(const Racional& r1, const Racional& r2)
+{
+  int numerador = r1.get_numerador() * r2.get_denominador() - r1.get_denominador() * r2.get_numerador();
+  int denominador = r1.get_denominador() * r2.get_denominador();
+  Racional out { numerador, denominador };
+  return out;
+}
+
+std::ostream& operator<<(std::ostream& os, const Racional& racional)
+{
+  os << racional.get_numerador() << "/" << racional.get_denominador();
+  return os;
+}
--- ejs/u3e02/main.cpp
+++ ejs/u3e02/main.cpp
@@ -6,27 +6,67 @@ int main()
 {
   std::cout << "u3e02" << std::endl;
 
-  Racional r1;
-  // 0/1
-  std::cout << r1.get_numerador() << "/" << r1.get_denominador() << "\n";
+  //  Racional r1;
+  //  // 0/1
+  //  std::cout << r1.get_numerador() << "/" << r1.get_denominador() << "\n";
+  //
+  //  Racional r2 { 2 };
+  //  // 2/1
+  //  std::cout << r2.get_numerador() << "/" << r2.get_denominador() << "\n";
+  //
+  //  Racional r3 { 3, 2 };
+  //  // 3/2
+  //  std::cout << r3.get_numerador() << "/" << r3.get_denominador() << "\n";
+  //
+  //  RacionalV2 r11;
+  //  // 0/1
+  //  std::cout << r11.get_numerador() << "/" << r11.get_denominador() << "\n";
+  //
+  //  RacionalV2 r22 { 2 };
+  //  // 2/1
+  //  std::cout << r22.get_numerador() << "/" << r22.get_denominador() << "\n";
+  //
+  //  RacionalV2 r33 { 3, 2 };
+  //  // 3/2
+  //  std::cout << r33.get_numerador() << "/" << r33.get_denominador() << "\n";
 
-  Racional r2 { 2 };
-  // 2/1
-  std::cout << r2.get_numerador() << "/" << r2.get_denominador() << "\n";
+  {
 
-  Racional r3 { 3, 2 };
-  // 3/2
-  std::cout << r3.get_numerador() << "/" << r3.get_denominador() << "\n";
+    Racional r1 { 1, 2 };
+    Racional r2 { 3, 4 };
+    //    Racional r3 = r1.sumar(r2);
+    Racional r3 = r1 + r2;
 
-  RacionalV2 r11;
-  // 0/1
-  std::cout << r11.get_numerador() << "/" << r11.get_denominador() << "\n";
+    std::cout << "sumar " << r3 << "\n";
+    //    std::cout.operator<<("sumar") << r3.get_numerador() << "/" << r3.get_denominador() << "\n";
+  }
 
-  RacionalV2 r22 { 2 };
-  // 2/1
-  std::cout << r22.get_numerador() << "/" << r22.get_denominador() << "\n";
+  {
+    Racional r1 { 1, 2 };
+    Racional r3 = r1 - r1;
+    std::cout << "restar " << r3 << "\n";
+    //    std::cout << "restar " << r3.get_numerador() << "/" << r3.get_denominador() << "\n";
+  }
 
-  RacionalV2 r33 { 3, 2 };
-  // 3/2
-  std::cout << r33.get_numerador() << "/" << r33.get_denominador() << "\n";
+  {
+    Racional r1 { 1, 2 };
+    std::cout << "incrementar antes " << r1 << "\n";
+    ++(++r1);
+    std::cout << "incrementar despues " << r1 << "\n";
+    Racional r2 { 1, 2 };
+    std::cout << "incrementar antes " << r2 << "\n";
+    r2++;
+    std::cout << "incrementar despues " << r2 << "\n";
+  }
+
+  {
+    Racional r1 { 1, 2 };
+    Racional r2 = r1 + 3;
+    std::cout << "sumar entero a racional " << r2 << "\n";
+  }
+
+  {
+    Racional r1 { 4, 9 };
+    std::cout << "simplificar " << r1.simplificar() << "\n";
+  }
 }
