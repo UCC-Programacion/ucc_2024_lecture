6c4d39defbc5c8a72cb6c7c8a2813070647beb06
Matias Volpe <kratico@users.noreply.github.com>
2024-05-20 11:42:57 -0300

    feat: agrega playground excepciones

---
+++ playground/05/Racional.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <cassert>
+#include <exception> // std::exception
+#include <iostream>
+#include <numeric>
+#include <stdexcept> // std::logic_error std::runtime_error
+
+class Racional {
+private:
+  int m_numerador;
+  int m_denominador;
+
+public:
+  Racional()
+    : Racional(0)
+  {
+  }
+
+  Racional(int numerador)
+    : Racional(numerador, 1)
+  {
+  }
+
+  Racional(int numerador, int denominador)
+    : m_numerador { numerador }
+    , m_denominador { denominador }
+  {
+
+    if (m_denominador == 0) {
+      // throw std::exception();
+      throw std::logic_error("denominador no puede ser 0");
+    }
+  }
+
+  //  Racional(int numerador = 0, int denominador = 1)
+  //    : m_numerador { numerador }
+  //    , m_denominador { denominador }
+  //  {
+  //    assert(denominador != 0);
+  //  }
+
+  int get_numerador() const
+  {
+    return m_numerador;
+  }
+
+  int get_denominador() const
+  {
+    return m_denominador;
+  }
+
+  Racional& operator++()
+  {
+    m_numerador += m_denominador;
+    return *this;
+  }
+
+  Racional operator++(int)
+  {
+    Racional temp = *this;
+    m_numerador += m_denominador;
+    return temp;
+  }
+
+  // TODO: implementar pre/post decrementar
+
+  Racional simplificar() const
+  {
+    int gcd = std::gcd(m_numerador, m_denominador);
+    return Racional { m_numerador / gcd, m_denominador / gcd };
+  };
+
+  //  void simplificar()
+  //  {
+  //    int gcd = std::gcd(m_numerador, m_denominador);
+  //    m_numerador = m_numerador / gcd;
+  //    m_denominador = m_denominador / gcd;
+  //  };
+};
+
+//  1/2 + 3/4
+// = (1*4 + 2*3) / 2*4
+Racional operator+(const Racional& r1, const Racional& r2)
+{
+  int numerador = r1.get_numerador() * r2.get_denominador() + r1.get_denominador() * r2.get_numerador();
+  int denominador = r1.get_denominador() * r2.get_denominador();
+  Racional out { numerador, denominador };
+  return out;
+}
+
+Racional operator+(const Racional& r1, int n)
+{
+  int numerador = r1.get_numerador() * 1 + r1.get_denominador() * n;
+  int denominador = r1.get_denominador() * 1;
+  Racional out { numerador, denominador };
+  return out;
+}
+
+Racional operator-(const Racional& r1, const Racional& r2)
+{
+  int numerador = r1.get_numerador() * r2.get_denominador() - r1.get_denominador() * r2.get_numerador();
+  int denominador = r1.get_denominador() * r2.get_denominador();
+  Racional out { numerador, denominador };
+  return out;
+}
+
+std::ostream& operator<<(std::ostream& os, const Racional& racional)
+{
+  os << racional.get_numerador() << "/" << racional.get_denominador();
+  return os;
+}
---
+++ playground/05/main.cpp
@@ -0,0 +1,36 @@
+#include "Racional.h"
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+
+int main()
+{
+  // https://hackingcpp.com/cpp/lang/exceptions.html
+
+  while (true) {
+    std::cout << "ingrese numerador: ";
+    int numerador;
+    std ::cin >> numerador;
+
+    std::cout << "ingrese denomindaor: ";
+    int denominador;
+    std ::cin >> denominador;
+
+    try {
+      throw std::runtime_error("runtime error >>>>>");
+      Racional racional { numerador, denominador };
+
+      std::cout << racional << "\n";
+      break;
+    } catch (std::logic_error const& e) {
+      std::cout << "error de logica: " << e.what() << "\n";
+      continue;
+    } catch (std::exception const& e) {
+      std::cout << "error: " << e.what() << "\n";
+      throw;
+    } catch (...) {
+      std::cout << "ocurrio un error\n";
+      throw;
+    }
+  }
+}
